4795c7dcdf8771b45e96ab0a5eb156e6cfa6387a
KrisMih <kristianmih15@gmail.com>
2025-02-28 10:00:31 +0200

    removed redeclarations

--- server.js
+++ server.js
@@ -4,57 +4,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-
-dotenv = require('dotenv');
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'css')));
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
-
-// User Schema
-const UserSchema = new mongoose.Schema({
-    username: String,
-    email: { type: String, unique: true },
-    password: String
-});
-const User = mongoose.model('User', UserSchema);
-
-// Routes for serving pages
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/bookpage', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'bookpage.html'));
-});
-
-app.get('/categ1', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'categ1.html'));
-});
-
-app.get('/categ2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'categ2.html'));
-});
-
-app.get('/categ3', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'categ3.html'));
-});
-
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -86,6 +35,22 @@ const User = mongoose.model('User', UserSchema);
 // Routes for serving HTML pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/bookpage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'bookpage.html'));
+});
+
+app.get('/categ1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'categ1.html'));
+});
+
+app.get('/categ2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'categ2.html'));
+});
+
+app.get('/categ3', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'categ3.html'));
 });
 
 app.get('/login', (req, res) => {
