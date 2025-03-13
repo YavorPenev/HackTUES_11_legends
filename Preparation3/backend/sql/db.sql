5a26cb0b67f46553ad0d5b9a1963400e2e75b76f
YavorPenev <yavorpen@gmail.com>
2025-03-13 21:49:26 +0200

    add table

--- Preparation3/backend/sql/db.sql
+++ Preparation3/backend/sql/db.sql
@@ -6,3 +6,9 @@ CREATE TABLE users (
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE article (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50)  NOT NULL,
+    article_text VARCHAR(6000) NOT NULL
+);
