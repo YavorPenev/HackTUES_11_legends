bc63edd748a1ad579a4e6fc1dd51d31710c7657d
YavorPenev <yavorpen@gmail.com>
2025-03-15 14:05:31 +0200

    adding elements

---
+++ elements/root/Backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Lab_007",
+    database: "root_test"
+})
+
+app.get('/', (re, res) => {
+    return res.json("From backend side");
+})
+
+app.get('/users', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+
+})
+
+app.listen(8090, () => {
+    console.log(`Server running on http://localhost:${8090}`);
+});
