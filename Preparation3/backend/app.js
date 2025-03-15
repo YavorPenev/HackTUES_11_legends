836a50f99a346cdd41688c09a474ed53bb72f5fa
YavorPenev <yavorpen@gmail.com>
2025-03-15 18:43:15 +0200

    dropdown add

--- Preparation3/backend/app.js
+++ Preparation3/backend/app.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
- password: "kris7504",
+ password: "Lab_007",
     database: "yourdatabase"
 });
 
