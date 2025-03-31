eb85d5e5a12859ccc69715af8210b77100d380a2
DeyanAzSum <deyan662@gmail.com>
2025-03-31 18:09:06 +0300

    localhost trqbva da e 8001

--- TUES_FEST/Preparation/Dein/TuesFest25/Preparation1/server.js
+++ TUES_FEST/Preparation/Dein/TuesFest25/Preparation1/server.js
@@ -38,17 +38,16 @@ app.post('/send-email', (req, res) => {
     let htmlContent = '';
     
     if (action === 'text') {
-        htmlContent = '<p>Произволен текст: Здравейте от нашия сайт!</p>';
+        htmlContent = '<p>Здравейте от нашия сайт!</p>';
     } else if (action === 'link') {
         const token = crypto.randomBytes(20).toString('hex');
         verificationLinks[token] = email;
-       // htmlContent = `<p>Щракнете <a href='http://localhost:8000/homepage/${token}'>тук</a> за достъп до сайта.</p>`;
-       htmlContent = `<p>Щракнете <a href='http://localhost:8001'>тук</a> за достъп до сайта.</p>`;// oprawih go weche te pra]a w nachalnata stranica, no towa s tokenite ne raboti
+       htmlContent = `<p>Цъкнете <a href='http://localhost:8001/homepage/${token}'>тук</a> за достъп до сайта.</p>`;
     } else if (action === 'text-link-image') {
         const token = crypto.randomBytes(20).toString('hex');
         verificationLinks[token] = email;
-        htmlContent = `<p>Произволен текст: Добре дошли!</p>
-                       <p>Щракнете <a href='http://localhost:8000/homepage/${token}'>тук</a> за достъп до сайта.</p>
+        htmlContent = `<p>Добре дошли!</p>
+                       <p>Щракнете <a href='http://localhost:8001/homepage/${token}'>тук</a> за достъп до сайта.</p>
                        <img src='https://via.placeholder.com/150' alt='Изображение'>`;
     }
     
